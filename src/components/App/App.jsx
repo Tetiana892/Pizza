f0097efe3f499d9fde03687df4aa27951c84e8ab
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-12-11 13:25:41 +0200

    add component sort popup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import css from './app.module.css';
-import { Header, Categories } from 'components';
+import { Header, Categories, SortPopup } from 'components';
 
 const App = () => {
   return (
@@ -14,32 +14,7 @@ const App = () => {
               onClickItem={name => console.log(name)}
               items={['Meat', 'Vegetarian', 'Grill', 'Sharp', 'Closed']}
             />
-            <div className={css.sort}>
-              <div className={css.sortlabel}>
-                <svg
-                  style={{ marginRight: 8 }}
-                  width="10"
-                  height="6"
-                  viewBox="0 0 10 6"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                    fill="#2C2C2C"
-                  />
-                </svg>
-                <b style={{ marginRight: 8 }}>Sort by:</b>
-                <span className={css.popularity}>Popularity</span>
-              </div>
-              <div className={css.sortpopup}>
-                <ul className={{ oveflow: 'hidden' }}>
-                  <li className={css.priceactive}>popularity</li>
-                  <li className={css.price}>price</li>
-                  <li className={css.price}>alphabet</li>
-                </ul>
-              </div>
-            </div>
+            <SortPopup items={['popularity', 'price', 'alphabet']} />
           </div>
           <h2 className={css.contenttitle}>All Pizzas</h2>
           <div className={css.contentitems}>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Button from './Button/button';
 import Categories from './Categories/categories';
 import Header from './Header/header';
+import SortPopup from './SortPopup/sortpopup';
 
-export { Button, Categories, Header };
+export { Button, Categories, Header, SortPopup };
